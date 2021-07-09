114c5a4a187049ae8966f811acd9d0f49fdf99fe
ikuromori <83898553+ikuromori@users.noreply.github.com>
2021-07-10 08:34:23 +0900

    Update maintain.php

--- maintain.php
+++ maintain.php
@@ -12,7 +12,7 @@
   
 <?php
 try {
-  $pdo = new PDO('mysql:dbname=ikuromori_a_db;charset=utf8;host=mysql57.ikuromori.sakura.ne.jp', 'ikuromori', 'akabane1');
+  $pdo = new PDO('mysql:dbname=ikuromori_a_db;charset=utf8;host=mysql57.ikuromori.sakura.ne.jp', 'ikuromori', '**********');
 } catch (PDOException $e) {
   exit('DbConnectError:'.$e->getMessage());
 }
